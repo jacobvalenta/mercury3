a0003e2bc6bc1650b2ff6746aa08fcf34fd1cdb1
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-03 18:33:56 -0500

    Basic Item

--- mercury3/items/models.py
+++ mercury3/items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	STATUS_CHOICES = (
+		('sale', "Sellable"),
+		('hold', "Hold")
+	)
+
+	make = models.CharField(max_length=48, blank=True, null=True)
+	model = models.CharField(max_length=48, blank=True, null=True)
+
+	status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+	def __str__(self):
+		return "{0} {1}".format(self.make if self.make else '', self.model).strip()
