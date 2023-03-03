6da1acb485eef02096a94bf709603654ac6401b8
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-03 18:33:20 -0500

    Hide Secret Key

--- mercury3/settings/base.py
+++ mercury3/settings/base.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9bt^#^bcd-g$wj%%cdhe=&_s^x$fnbh5p=1hfq%mwq5ev233+l'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
