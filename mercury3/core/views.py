2837af215e6e5c10b41c416c7cf2d9464353c2f2
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-04 15:33:21 -0500

    Main page

--- mercury3/core/views.py
+++ mercury3/core/views.py
@@ -1,3 +1,6 @@
+from django.views.generic import TemplateView
+
 from django.shortcuts import render
 
-# Create your views here.
+class MainPageView(TemplateView):
+	template_name = "main_page.html"
