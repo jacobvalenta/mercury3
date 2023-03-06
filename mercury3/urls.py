f060d03fbbae8c24edc9eff98c9765233f7a1ef4
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-05 19:15:11 -0500

    Update urls.py

--- mercury3/urls.py
+++ mercury3/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mercury3.core.urls')),
-    path('', include('mercury3.customers.urls'))
+    path('', include('mercury3.customers.urls')),
+    path('items/', include(('mercury3.items.urls', 'items'), namespace="items")),
+    path('transactions/', include(('mercury3.transactions.urls', 'transactions'), namespace="transactions"))
 ]
