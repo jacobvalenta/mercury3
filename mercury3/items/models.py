26eae628e12ad781451bdaee1cb704eaa55c3307
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-05 19:14:30 -0500

    Add Item.price

--- mercury3/items/models.py
+++ mercury3/items/models.py
@@ -9,6 +9,10 @@ class Item(models.Model):
 	make = models.CharField(max_length=48, blank=True, null=True)
 	model = models.CharField(max_length=48, blank=True, null=True)
 
+	price_in = models.DecimalField(max_digits=9, decimal_places=2)
+	price = models.DecimalField(max_digits=9, decimal_places=2)
+	price_out = models.DecimalField(max_digits=9, decimal_places=2, blank=True, null=True)
+
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
 	def __str__(self):
