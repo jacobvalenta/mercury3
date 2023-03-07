a13009bc013b37eb8093a88dbbde83f5d0700e12
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-07 10:44:25 -0500

    transaction_type

--- mercury3/transactions/models.py
+++ mercury3/transactions/models.py
@@ -7,6 +7,20 @@ class TransactionItem(models.Model):
 	price = models.DecimalField(max_digits=9, decimal_places=2)
 
 class Transaction(models.Model):
+	BUY = "buy"
+	PAWN = "pawn"
+	SALE = "sale"
+	LAYAWAY = "layaway"
+
+	TRANSACTION_TYPE_CHOICES = (
+		(BUY, "Buy"),
+		(PAWN, "Pawn"),
+		(SALE, "Sale"),
+		(LAYAWAY, "Layaway")
+	)
+
+	transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
+
 	customer = models.ForeignKey('customers.Customer', on_delete=models.CASCADE, blank=True, null=True)
 	items = models.ManyToManyField('items.Item', through="transactions.TransactionItem")
 
