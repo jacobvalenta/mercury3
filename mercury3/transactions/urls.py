6aec15d5082171c4e70d4ebf82dcbffa25053561
Jacob Valenta <jacob.valenta@gmail.com>
2023-03-15 15:19:28 -0400

    rename transaction urls

--- mercury3/transactions/urls.py
+++ mercury3/transactions/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import InTransactionCreateView, OutTransactionCreateView, PayOrRedeemPawnView, TransactionDetailView
 
 urlpatterns = [
-    path('create_in/', InTransactionCreateView.as_view(), name="create-in-zero-info"),
-    path('create_out/', OutTransactionCreateView.as_view(), name="create-out-zero-info"),
-    path('pay/', PayOrRedeemPawnView.as_view(), {"type": "pay"}, name="pay"),
-    path('redeem/', PayOrRedeemPawnView.as_view(), {"type": "redeem"}, name="redeem"),
+    path('create_in/', InTransactionCreateView.as_view(), name="create-in"),
+    path('create_out/', OutTransactionCreateView.as_view(), name="create-out"),
+    path('pay/', PayOrRedeemPawnView.as_view(), kwargs={"type": "pay"}, name="pay"),
+    path('redeem/', PayOrRedeemPawnView.as_view(), kwargs={"type": "redeem"}, name="redeem"),
     path('<int:pk>/', TransactionDetailView.as_view(), name="detail"),
 ]
